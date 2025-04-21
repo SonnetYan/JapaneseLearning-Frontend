50d8da3094e25793502a3cfbd565055398b52e4e
SonnetYan <yanmingxi7@gmail.com>
2025-04-21 00:12:26 -0400

    try test 1

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ export const api = axios.create({
   headers: {
     'Content-Type': 'application/json'
   },
-  withCredentials: true
+  withCredentials: false
 });
 
 // Add response interceptor for better error handling
